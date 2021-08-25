6b71046f654605fa245ebb3aba99dcd20a10fce0
johnzhengss <john.zheng@smoothstack.com>
2021-08-25 07:13:45 -0500

    BB-81 Eureka_Service update service name

---
+++ demo_service/src/main/java/com/ss/demo_service/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.ss.demo_service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/index")
+    public String test(){
+        return "hello index";
+    }
+}
